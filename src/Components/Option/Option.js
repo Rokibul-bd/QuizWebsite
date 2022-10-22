7b8124ae6e9ee187e803d45f9311e0effd17d5f4
Rokibul-bd <mdrokbiulislamcmt176@gmail.com>
2022-10-22 18:31:20 +0600

    create option Components

---
+++ src/Components/Option/Option.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Option = ({ option }) => {
+    console.log(option)
+    return (
+        <div className='border-teal-400 border p-2 m-2 rounded-sm'>
+            <p> <input className='mx-2' type="radio" value={option} />{option}</p>
+        </div>
+    );
+};
+
+export default Option;
--- src/Components/Questions/Questions.js
+++ src/Components/Questions/Questions.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import Option from '../Option/Option';
 
 const SingleQuiz = ({ questionQuiz }) => {
     const { question, options } = questionQuiz
     return (
         <div className='border border-teal-300 mb-8 p-12 rounded-md shadow-lg'>
             <h3 className='my-8'>{question}</h3>
-            {
-                options.map(option => <div>this </div>)
-            }
+            <div className='md:grid grid-cols-2'>
+                {
+                    options.map(option => <Option key={option.id} option={option}></Option>)
+                }
+            </div>
         </div>
     );
 };
