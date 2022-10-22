f49edbaa313827ada34232b8fcd3825ad98a58ec
Rokibul-bd <mdrokbiulislamcmt176@gmail.com>
2022-10-22 17:45:35 +0600

    finished course components

--- src/Components/Courses/Courses.js
+++ src/Components/Courses/Courses.js
@@ -8,7 +8,7 @@ const Courses = () => {
     return (
         <div>
             <h2 className='text-4xl text-center text-red-500 my-24'>This is Courses Components</h2>
-            <div className='md:grid grid-cols-4'>
+            <div className='md:grid grid-cols-4 mx-8'>
                 {
                     data.map(course => <Course key={course.id} course={course}></Course>)
                 }
--- src/Components/course/Course.js
+++ src/Components/course/Course.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 const Course = ({ course }) => {
-    const { name, logo } = course
+    const { name, logo, total } = course
     return (
-        <div>
-            <img src={logo} alt="" />
-            <h3>{name}</h3>
+        <div className='bg-slate-500 shadow-md rounded-lg p-4 mx-4 text-white mb-2 md:mb-0'>
+            <h3 className='text-2xl'>Total quiz of {name} : {total}</h3>
+            <img className='my-4' src={logo} alt="" />
+            <div className='flex justify-between'>
+                <h3 className='text-2xl'>{name}</h3>
+                <button className='bg-white shadow-md rounded-md text-black px-4 hover:bg-slate-300'>Start Practice</button>
+            </div>
         </div>
     );
 };
